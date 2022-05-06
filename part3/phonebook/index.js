494435592585755f86134483814f375650dd5c1a
Malin Häggblom <malin.haggblom@centria.fi>
2022-05-06 13:54:15 +0300

    Part 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -61,16 +61,22 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
-      error: 'name missing' 
+      error: 'The name or number missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
     })
   }
 
   const person = {
+    id: generateId(),
     name: body.name,
     number: body.number,
-    id: generateId(),
   }
 
   persons = persons.concat(person)
