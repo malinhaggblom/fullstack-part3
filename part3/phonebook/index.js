a7ff5216dbfbcc849f5b7334be4d5db1d4229e7c
Malin Häggblom <malin.haggblom@centria.fi>
2022-05-06 11:41:35 +0300

    Part 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,14 +24,20 @@ let persons = [
     }
 ]
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
   })
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const info  = `<div> <p>Phonebook has info for ${persons.length} people</p></div>
+    <p>${new Date().toString()}</p>`
+  response.send(info)
 })
   
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
