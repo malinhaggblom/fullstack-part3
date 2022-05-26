d766165e5da6786c82f7ef48b86df325f5a0ca2c
Malin Häggblom <malin.haggblom@centria.fi>
2022-05-26 21:44:24 +0300

    Part 3.11

---
+++ part3/phonebook/backend/build/precache-manifest.5d7b264345c16549b2d0fb779acab782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3b02c68e6da4e1c317",
+    "url": "/static/css/main.d19ff6d9.chunk.css"
+  },
+  {
+    "revision": "5d3b02c68e6da4e1c317",
+    "url": "/static/js/main.20b6ec1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f05c532203113a354f91",
+    "url": "/static/js/2.7a30a521.chunk.js"
+  },
+  {
+    "revision": "33d86b1f9d8fc15e408fcc7aa36a9197",
+    "url": "/index.html"
+  }
+];
