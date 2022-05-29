5d5a71db1156220c56684dab7f3905eabe138905
Malin Häggblom <malin.haggblom@centria.fi>
2022-05-29 13:37:26 +0300

    Part 3.15

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -28,17 +28,37 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Persons.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Persons.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  console.log(request.params)
+  Persons.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  Persons.findByIdAndUpdate(request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
