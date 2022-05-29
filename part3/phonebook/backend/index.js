f74da0706743c08715eb963a6487e478d8fa14df
Malin Häggblom <malin.haggblom@centria.fi>
2022-05-29 13:29:20 +0300

    Part 3.14

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -14,41 +14,11 @@ morgan.token('post', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck",
-      number: "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
   Persons.find({}).then(persons => {
     response.json(persons)
   })
 })
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
