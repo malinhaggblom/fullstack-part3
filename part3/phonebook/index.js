864c8245ad8dc68b3df055484d3672af09940e15
Malin Häggblom <malin.haggblom@centria.fi>
2022-06-02 11:12:49 +0300

    update

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,18 +14,14 @@ morgan.token('post', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-app.get('/api/persons', (request, response) => {
-  Persons.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response, next) => {
-  response.json(persons)
+app.get('/api/persons', (request, response) => {
+  Persons.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
